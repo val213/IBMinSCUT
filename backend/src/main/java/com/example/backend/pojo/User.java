8fbe78038dfc45450eff19c77e6615bb034d9c12
miaojiayi <2448735803@qq.com>
2024-09-10 15:32:53 +0800

    update

--- backend/src/main/java/com/example/backend/pojo/User.java
+++ backend/src/main/java/com/example/backend/pojo/User.java
@@ -3,6 +3,12 @@ package com.example.backend.pojo;
 
 import jakarta.persistence.*;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 
 import javax.persistence.*;
